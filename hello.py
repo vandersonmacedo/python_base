293ec7190301b154eaa25af0fcc528c250790ecc
vanderson lopes macedo <vanderson.progress@gmail.com>
2025-05-24 23:42:15 -0300

    Implementamos Hello World muti linguas

--- hello.py
+++ hello.py
@@ -1,3 +1,45 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
 
-print("Hello, World!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem      .
+correspondente.
+
+usage:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+
+    export LANG=pt_BR
+
+
+Execucao:
+
+    python3 hello.py
+    ou
+    ./hello.py
+    
+"""
+__version__ = "0.0.1"
+__author__ = "Vanderson Macedo"
+__license__ = "Unlicense"
+
+# Dunder = __
+
+# Este programa imprime Hello World
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+    
+
+
+print(msg)
